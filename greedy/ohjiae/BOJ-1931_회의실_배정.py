936e746e84b21ef77ac8ea87cc8691a11ffffaf0
OH JI AE <ohjiae.dev@gmail.com>
2022-03-17 21:04:41 +0900

    Greedy: [#37] ohjiae - 회의실 배정

--- greedy/ohjiae/BOJ-1931_회의실_배정.py
+++ greedy/ohjiae/BOJ-1931_회의실_배정.py
@@ -1,6 +1,6 @@
 from sys import stdin
-N = int(input())
-schedules = [list(map(int, stdin.readline().split())) for _ in range(N)]
+n = int(input())
+schedules = [list(map(int, stdin.readline().split())) for _ in range(n)]
 schedules.sort(key = lambda x : (x[1],x[0]))
 end = 0 
 cnt = 0
