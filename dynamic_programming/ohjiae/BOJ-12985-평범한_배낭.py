c83c71a7edf9be002439fbdb87a2f313e6a924e6
ohjiae <ohjiae.dev@gmail.com>
2022-03-24 20:57:06 +0900

    DynamicProgramming: [#48] ohjiae - 평범한 배낭

--- dynamic_programming/ohjiae/BOJ-12985-평범한_배낭.py
+++ dynamic_programming/ohjiae/BOJ-12985-평범한_배낭.py
@@ -3,15 +3,17 @@ from sys import stdin
 input = stdin.readline
 
 N, K = list(map(int, input().split()))
-A = []
-bag = 0
+dp = {}
+mv = 0
 for _ in range(N):
     W, V = list(map(int, input().split()))
     if V != 0 and W <= K:
-        AW = list(A[i][0] for i in range(len(A)))
-        if W in AW:
-            idx = AW.index(W)
-            AW[idx] = (W,max(AW[idx][1], V))
-        else: A.append((W, V))
+        if W in dp:
+            dp[W] = max(dp[W], V)
+        else:
+            dp[W] = V
+        if dp[K-W] in dp:
+            mv = max(dp[K-W]+dp[W], mv)
+        
 
-
+print(max(dp.values()))
