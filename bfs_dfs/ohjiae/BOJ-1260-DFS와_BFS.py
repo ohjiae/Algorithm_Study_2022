640b807a62d6b177eaeb9cae66d5e840fbde1643
OH JI AE <ohjiae.dev@gmail.com>
2022-03-31 16:15:03 +0900

    BFS/DFS: [#62] ohjiae - DFS와 BFS

--- bfs_dfs/ohjiae/BOJ-1260-DFS와_BFS.py
+++ bfs_dfs/ohjiae/BOJ-1260-DFS와_BFS.py
@@ -1,34 +1,39 @@
-import sys
 from collections import deque
+import sys
+input = sys.stdin.readline
 N, M, V = map(int,input().split())
-# input = sys.stdin.readline
-
-graph = {}
-for _ in range(M):
-    p, c = map(int,input().split())
-    if p in graph: graph[p].append(c)
-    else: graph[p] = [c]
-
-dis = [False] * (N + 1)
-def dfs(graph,V):
-    dis[V] = True
-    print(V,end=' ')
-    if V in graph:
-        for i in graph[V]:
-            if not dis[i]:
-                dfs(graph,i)
-
-bis = [False] * (N + 1)
-q=deque(V)
-def bfs(graph,V):
+graph = [[] for _ in range(N+1)]
+vis = [False for _ in range(N+1)]
+res = []
+for i in range(M):
+    s,e = map(int,input().split())
+    graph[s].append(e)
+    graph[e].append(s)
+for j in graph:
+    if len(j)>1:
+        j.sort()
+stack = deque()
+def dfs(V):
+    res.append(V)
+    vis[V] = True
+    stack.append(V)
+    node = stack.pop()
+    for i in graph[node]:
+        if not vis[i]:
+            dfs(i)
+dfs(V)
+print(*res)
+res = []
+vis = [False for _ in range(N+1)]
+q = deque([V])
+def bfs():
     while q:
         node = q.popleft()
-        bis[node] = True
-        print(node, end=' ')
-        if node in graph and len(graph[node])!=0:
-            for i in graph[node]:
-                if not bis[i]:
-                    q.append(i)     
-
-dfs(graph,V)
-bfs(graph,V)
+        if not vis[node]:
+            res.append(node)
+            vis[node] = True
+            for n in graph[node]:
+                if not vis[n]:
+                    q.append(n)
+bfs()
+print(*res)
