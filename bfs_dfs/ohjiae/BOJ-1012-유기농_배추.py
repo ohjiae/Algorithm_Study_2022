1ceef2745b7ecd31905f31013d500a862976c25e
ohjiae <ohjiae.dev@gmail.com>
2022-04-04 21:29:30 +0900

    BFS/DFS: [#73] ohjiae - 유기농 배추

---
+++ bfs_dfs/ohjiae/BOJ-1012-유기농_배추.py
@@ -0,0 +1,25 @@
+#from sys import stdin
+#input =  stdin.readline
+
+def dfs(g,x,y):
+    g[x][y] = 0
+    for i in [(-1,0),(0,1),(0,-1),(1,0)]:
+        if 0<= x+i[0] <N and 0<= y+i[1] <M and g[x+i[0]][y+i[1]] == 1:
+            dfs(g,x+i[0],y+i[1])
+
+T = int(input())
+cnt = 0
+for i in range(T):
+    M,N,K = map(int,input().split())
+    graph = [[0 for _ in range(M)] for _ in range(N)]
+    for where in range(K):
+        X,Y = map(int,input().split())
+        graph[X][Y] = 1
+    for col in range(M):
+        for row in range(N):
+            if graph[col][row] == 1:
+                cnt += 1
+                print(f'T={t},col={col},row={row},cnt={cnt},gcr={graph[col][row]}')
+                dfs(graph,col,row)
+
+
