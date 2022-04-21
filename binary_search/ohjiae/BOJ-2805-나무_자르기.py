94ae8d020ea2c137fee67bf596013f9e62689dbd
ohjiae <ohjiae.dev@gmail.com>
2022-04-21 20:56:03 +0900

    BinarySearch: [#94] ohjiae - 나무 자르기

--- binary_search/ohjiae/BOJ-2805-나무_자르기.py
+++ binary_search/ohjiae/BOJ-2805-나무_자르기.py
@@ -1,16 +1,24 @@
 import sys
-input = sys.stdin.readline()
-N,M = int(input().split())
+input = sys.stdin.readline
+N,M = map(int,input().split())
 tree = sorted(list(map(int,input().split())),reverse=True)
 H = tree[0]-M
-gain = 0
-
-# for문 시작
-# H만큼 자르면서 gain+
-# gain 채워지면 break
-# for문 끝
-# gain이
-for i in range(tree):
-    gain += i-cut
-    if gain >=
-print(tree)
+#기준 미달 다빼고 시작
+for i in range(N):
+    if tree[i]<=H:
+        tree = tree[:i]
+        break
+#반으로 나누면서 적정지점 서치
+tree.reverse()
+start, end = tree[-1],tree[0]
+while start<=end:
+    mid = (start+end)//2
+    gain = 0
+    for i in tree:
+        if i>= mid:
+            gain += i-mid
+    if gain >= M:
+        start = mid + 1
+    else:
+        end = mid-1
+print(end)
