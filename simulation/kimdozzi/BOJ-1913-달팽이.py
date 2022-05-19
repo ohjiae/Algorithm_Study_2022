b7d7692a6905427e2e48eb7ef89e0bd214630f58
DoHyung Kim <neo5188@gmail.com>
2022-05-20 00:28:55 +0900

    05/19 [BOJ]1913-달팽이.py

--- simulation/kimdozzi/BOJ-1913-달팽이.py
+++ simulation/kimdozzi/BOJ-1913-달팽이.py
@@ -1,30 +1,36 @@
-N = int(input())
-target = int(input())
-targetX, targetY = 0, 0
-dx = [1, 0, -1, 0]
-dy = [0, 1, 0, -1]
-matrix = [[0 for _ in range(N)] for _ in range(N)]
-value = (N ** 2)
-direction = 0
-x, y = 0, 0
+def solution(n):
+    global targetX, targetY
+    dx = [1, 0, -1, 0]
+    dy = [0, 1, 0, -1]
+    matrix = [[0 for _ in range(n)] for _ in range(n)]
 
-matrix[x][y] = value
-value -= 1
+    # 0,0 에서 초기화
+    value = n ** 2
+    direction = 0
+    x, y = 0, 0
+    matrix[x][y] = value
+    value -= 1
 
-while value > 0:
-    nx = x + dx[direction]
-    ny = y + dy[direction]
+    while value > 0:
+        nx = x + dx[direction]
+        ny = y + dy[direction]
 
-    if N <= nx < 0 and N <= ny < 0 and matrix[nx][ny] == 0:
-        matrix[nx][ny] = value
-        if value == target:
-            targetX, targetY = nx, ny
-        x, y = nx, ny
-        value -= 1
-    else:
-        direction = (direction + 1) % 4
+        if (0 <= nx < n) and (0 <= ny < n) and matrix[nx][ny] == 0:
+            matrix[nx][ny] = value
+            if value == target:
+                targetX, targetY = nx, ny
+            x, y = nx, ny
+            value -= 1
+        else:
+            direction = (direction + 1) % 4
+    return matrix
 
-for row in matrix:
-    print(*row)
 
-print(targetX + 1, targetY + 1)
+if __name__ == "__main__":
+    n = int(input())
+    target = int(input())
+    targetX, targetY = 0, 0
+    matrix = solution(n)
+    for i in matrix:
+        print(*i)
+    print(targetX+1, targetY+1)
