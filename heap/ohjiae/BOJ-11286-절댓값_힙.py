6920b5a0940bb59388735afbbb874488bfcfacde
OH JI AE <ohjiae.dev@gmail.com>
2022-05-27 10:44:23 +0900

    Heap: [#137][#138] ohjiae - 문제풀이

--- heap/ohjiae/BOJ-11286-절댓값_힙.py
+++ heap/ohjiae/BOJ-11286-절댓값_힙.py
@@ -1,14 +1,21 @@
-from collections import deque
-heap = deque()
-for _ in range(int(input())):
-    x = int(input())
-    heap.append(x)
-    #데큐엔 sort가없는데..
-    print(f'x={x},min={heap[0]}')
-    if x == 0:
-        if len(
-                heap)==0:
-            print(0)
-        else :
-            print(
-                heap.popleft())
+import sys
+import heapq as hq
+# hl = []
+# for i in range(int(input())):
+#     x = int(sys.stdin.readline().rstrip())
+#     if x == 0 :
+#         print(hq.heappop(hl)[1] if hl else 0)
+#     else : hq.heappush(hl,(abs(x),x))
+
+
+import heapq
+
+# 최소힙 리스트
+heap_list = [3, 7, 1, 5, 9]
+heapq.heapify(heap_list)
+
+# 원소 삭제
+heapq.heappop(heap_list)  # 리턴 후 삭제됨, list의 pop처럼.
+
+# 확인
+print(heap_list)
